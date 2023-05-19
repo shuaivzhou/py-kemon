9fa482314a8962d3ba78d150163512896bfcec06
Shuai Zhou <shuaivzhou@berkeley.edu>
2023-05-19 00:02:06 -0500

    initial commit

---
+++ tiled/BW_PublicOutside_New.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="BW_PublicOutside_New" tilewidth="16" tileheight="16" tilecount="16064" columns="16">
+ <image source="BW_PublicOutside_New.png" width="256" height="16064"/>
+</tileset>
